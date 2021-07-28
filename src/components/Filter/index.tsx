3c48889c96a3bf4d1dfd7342694b4d285d587b52
castelanlan <gabrielmcastelan@gmail.com>
2021-07-28 15:50:16 -0300

    eval('git add .')

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { ReactNode } from 'react';
+
+import { Container } from './styles';
+
+interface FilterProps {
+  children: ReactNode;
+}
+
+const Filter: React.FC = () => {
+  return (
+    <Container>
+    </Container>
+  );
+};
+
+export default Filter;
